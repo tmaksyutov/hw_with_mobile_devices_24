600c8a6c274dad30bcc00197c0677579f7ba4ab6
TMaksyutov <dadojkeee@mail.ru>
2023-01-11 19:30:14 +0300

    fix tests

--- src/test/java/tests/WikipediaAndroidTests.java
+++ src/test/java/tests/WikipediaAndroidTests.java
@@ -3,136 +3,42 @@ package tests;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
 public class WikipediaAndroidTests extends TestBase {
 
     @Test
-    void addLanguageOnFirstScreen() {
+    void onBoardingStepsTest() {
 
-        step("Кликаю добавить язык", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/addLangContainer")).click();
-        });
-
-        step("Кликаю добавить язык в меню языков", () -> {
-            $$(AppiumBy.id("org.wikipedia.alpha:id/wiki_language_title")).last().click();
-        });
-
-        step("Выбираю Русский", () -> {
-            $(AppiumBy.xpath("//android.widget.TextView[@text='Русский']")).click();
-        });
-
-        step("Проверяю, что в списке есть Русский", () -> {
-            $(AppiumBy.xpath("//android.widget.TextView[@text='Русский']"))
-                    .shouldHave(attribute("text", "Русский"));
-        });
-
-        step("Возвращаюсь назад", () -> {
-            $(AppiumBy.className("android.widget.ImageButton")).click();
-        });
-
-        step("Проверяю, что в списке есть Русский", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/languagesList"))
-                    .$$(AppiumBy.id("org.wikipedia.alpha:id/option_label"))
-                    .shouldHave(size(2));
-        });
-    }
-
-    @Test
-    void checkTextOnSecondScreen() {
-
-        String textOnScreenTwo = "Dive down the Wikipedia rabbit hole with a constantly updating Explore feed. \n" +
-                "Customize the feed to your interests – whether it’s learning about historical events On this day, or rolling the dice with Random.";
-
-        step("Скролю на второй экран", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-        });
-
-        step("Проверяю текст на экране", () -> {
+        step("Check that the text 'The Free Encyclopedia …in over 300 languages' is visible", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(attribute("text", "New ways to explore"));
-            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView"))
-                    .shouldHave(attribute("text", textOnScreenTwo));
-        });
-    }
-
-    @Test
-    void checkTextOnThirdScreen() {
-
-        String textOnScreenThree = "You can make reading lists from articles you want to read later, even when you’re offline. \n" +
-                "Login to your Wikipedia account to sync your reading lists. Join Wikipedia";
-
-        step("Скролю на третий экран", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+                    .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
         });
 
-        step("Проверяю текст на экране", () -> {
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check that the text 'New ways to explore' is visible", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(attribute("text", "Reading lists with sync"));
-            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView"))
-                    .shouldHave(attribute("text", textOnScreenThree));
-        });
-    }
-
-    @Test
-    void checkTextOnFourthScreen() {
-
-        String textOnScreenFour = "Help make the app better by letting us know how you use it. Data collected is anonymous. Learn more";
-
-        step("Скролю на третий экран", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+                    .shouldHave(text("New ways to explore"));
         });
 
-        step("Проверяю текст на экране", () -> {
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check that the text 'Reading lists with sync' is visible", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(attribute("text", "Send anonymous data"));
-            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView"))
-                    .shouldHave(attribute("text", textOnScreenFour));
+                    .shouldHave(text("Reading lists with sync"));
         });
 
-        step("Выключаю свитч отправки данных", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/switchView")).click();
-        });
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
 
-        step("Проверяю, что свитч отправки данных выключен", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/switchView"))
-                    .shouldHave(attribute("checked", "false"));
+        step("Check that the text 'Send anonymous data' is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Send anonymous data"));
         });
-
-        step("Нажимаю GET STARTED", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_done_button")).click();
-        });
-
-        step("Проверяю, что открывается главный экран", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/main_toolbar_wordmark")).shouldBe();
-        });
-
-    }
-
-    @Test
-    void searchTest() {
-
-        step("Закрываю экран настроек", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
-        });
-        step("Кликаю по элементу поиска", () -> {
-            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        });
-        step("Ввожу текст в поле поиска", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
-                    .setValue("BrowserStack");
-        });
-
-        step("Проверяю, что результатов поиска больше 0", () ->
-                $$(AppiumBy.className("android.widget.TextView"))
-                        .shouldHave(sizeGreaterThan(0)));
     }
 }
